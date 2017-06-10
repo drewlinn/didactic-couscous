aa135f00130a6e8bb1a8f6370101b83819a08528
Andrew <expandrew@gmail.com>
2017-06-09 19:20:03 -0700

    add pages and form for the user to add new clients and stylists.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -20,6 +20,30 @@ namespace HairSalon
         List<Stylist> AllStylist = Stylist.GetAll();
         return View["stylists.cshtml", AllStylist];
       };
+      Get["/client/new"] = _ =>  {
+        List<Stylist> AllStylist = Stylist.GetAll();
+        return View["/add_client.cshtml", AllStylist];
+      };
+      Get["/stylist/new"] = _ =>  {
+        return View["/add_stylist.cshtml"];
+      };
+      Post["/clients"]= _ =>{
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["client-phone"], Request.Form["client-email"], Request.Form["stylist_id"]);
+        var selectedStylist = Stylist.Find(newClient.GetStylistId());
+        newClient.Save();
+        model.Add("stylist", selectedStylist);
+        model.Add("client", newClient);
+        return View["successfully_added.cshtml", model];
+      };
+      Post["/stylists"]= _ =>{
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-phone"], Request.Form["stylist-email"]);
+        var selectedStylist = Stylist.Find(newStylist.GetId());
+        newStylist.Save();
+        model.Add("stylist", newStylist);
+        return View["successfully_added.cshtml", newStylist];
+      };
     }
   }
 }
